9572a23f1b06ada8f23f21c526080a077fa58e67
Ritesh Kadmawala <ritesh.kadmawala@xapo.com>
2022-04-24 12:00:28 +0530

    :zap: [examples] add an examples dir and a simple 'hello_world' example

---
+++ examples/hello_world.py
@@ -0,0 +1,18 @@
+import time
+
+import click
+
+
+@click.command()
+@click.argument("msg_list", nargs=-1)
+@click.option("-s", "--sleep", type=click.FLOAT, default=0.5)
+def hello(msg_list: list[str], sleep: float):
+    print("Hello\n")
+    for m in msg_list:
+        time.sleep(sleep)
+        print(m)
+
+
+if __name__ == "__main__":
+    print("Run Main")
+    hello()
